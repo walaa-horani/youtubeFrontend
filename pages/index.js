918cee9f574073233e5b4a95f19368371a024f3a
unknown <walaa.horani@hotmail.com>
2024-09-29 13:53:28 +0300

    Changes in the submodule

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p><a href="/youtubeAnalytics">Go to YouTube Analytics Page</a></p>
+      </div>
+    );
+  }
+  
