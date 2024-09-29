f5d58cc8e0ff6889a53e3669c504ad58867bb14f
Your Name <walaa.horani@hotmail.com>
2024-09-29 18:48:02 +0300

    fist

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import { Link } from '@mui/material';
-
+import { Link as MuiLink } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-      <div>
-        <h1>Welcome to the Home Page</h1>
-       <Link to="/youtubeAnalytics">Go to YouTube Analytics Page</Link>
-      </div>
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <MuiLink component={Link} to="/youtubeAnalytics">
+                Go to YouTube Analytics Page
+            </MuiLink>
+        </div>
     );
-  }
-  
+}
