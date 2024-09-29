95480e83852feeb09c73b6c2126d09b4e566b3ea
Your Name <walaa.horani@hotmail.com>
2024-09-29 18:44:17 +0300

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Link } from '@mui/material';
+
+
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p><Link yo="/youtubeAnalytics">Go to YouTube Analytics Page</Link></p>
+      </div>
+    );
+  }
+  
